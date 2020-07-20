cc2bc9bebd7f8b56b5de10adc9d244f94253e508
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2020-07-20 16:11:10 +0700

    BMK Version

---
+++ LoginSupermarketW6/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using LoginSupermarketW6.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace LoginSupermarketW6.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CustomersController : Controller
+    {
+        private SupermarketEntities db = new SupermarketEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = db.Customers.Include(c => c.Status1);
+            customers = customers.Where(c => c.Status == 0);
+            return View(customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
